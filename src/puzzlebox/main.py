ea96e00c7ec111aac1ed818c065d5e025d1bf97f
psend <psenderski94@gmail.com>
2026-09-04 10:57:54 +0200

    feat: log application startup

--- src/puzzlebox/main.py
+++ src/puzzlebox/main.py
@@ -1,10 +1,17 @@
+import logging
+
 from puzzlebox.cli import app
 from puzzlebox.logging import configure_logging
+
+logger = logging.getLogger(__name__)
 
 
 def main() -> None:
     """Start the PuzzleBox command-line application."""
     configure_logging()
+
+    logger.info("PuzzleBox started")
+
     app()
 
 
